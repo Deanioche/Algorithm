b1f65434984620d88776d6efdc5a0edf5eb8667c
Soksurim <briochefavori@gmail.com>
2021-06-05 22:42:37 +0900

    solving Algorithm "camo"

---
+++ JS/camoflague.js
@@ -0,0 +1,8 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42578
+
+var clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]];
+
+function solution(clothes) {
+    var answer = 0;
+    return answer;
+}
